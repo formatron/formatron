8408cf9c4c72353abf2d60e89d4e5309715798d5
Peter Halliday <pghalliday@gmail.com>
2015-09-18 16:38:12 +0200

    tests for the knife library

---
+++ lib/formatron/util/kernel_helper.rb
@@ -0,0 +1,15 @@
+class Formatron
+  module Util
+    # wrapper for shelling out calls so that we can still test
+    module KernelHelper
+      # :nocov:
+      def self.shell(command)
+        `#{command}`
+      end
+      def self.success?
+        $CHILD_STATUS.success?
+      end
+      # :nocov:
+    end
+  end
+end
--- lib/formatron/util/knife.rb
+++ lib/formatron/util/knife.rb
@@ -15,7 +15,7 @@ class Formatron
         @key_file.write(key)
         @key_file.close
         @knife_file = Tempfile.new('knife')
-        @knife_file.write <<-EOH
+        @knife_file.write <<-EOH.gsub(/^\s{8}/, '')
           chef_server_url '#{server_url}/organizations/#{organization}'
           node_name '#{user}'
           client_key '#{@key_file.path}'
@@ -30,10 +30,10 @@ class Formatron
       end
 
       def create_environment(environment)
-        `knife environment show #{environment} -c #{@knife_file.path}`
         # rubocop:disable Metrics/LineLength
-        `knife environment create #{environment} -c #{@knife_file.path} -d '#{environment} environment created by formatron'` unless $CHILD_STATUS.success?
-        fail CreateEnvironmentError, "failed to create opscode environment: #{environment}" unless $CHILD_STATUS.success?
+        Formatron::Util::KernelHelper.shell "knife environment show #{environment} -c #{@knife_file.path}"
+        Formatron::Util::KernelHelper.shell "knife environment create #{environment} -c #{@knife_file.path} -d '#{environment} environment created by formatron'" unless Formatron::Util::KernelHelper.success?
+        fail CreateEnvironmentError, "failed to create opscode environment: #{environment}" unless Formatron::Util::KernelHelper.success?
         # rubocop:enable Metrics/LineLength
       end
     end
