621135c67537363614404aca9121699378a3bdde
Peter Halliday <pghalliday@gmail.com>
2015-10-21 18:49:45 +0200

    almost there with bootstrap cli test

--- lib/formatron/generators/bootstrap/cli.rb
+++ lib/formatron/generators/bootstrap/cli.rb
@@ -4,15 +4,69 @@ module Formatron
   module Generators
     module Bootstrap
       # CLI command for bootstrap generator
+      # rubocop:disable Metrics/ModuleLength
       module CLI
+        # rubocop:disable Metrics/MethodLength
         def bootstrap_options(c)
           c.option '-n', '--name STRING', 'The name for the configuration'
+          c.option(
+            '-s',
+            '--s3-bucket STRING',
+            'The S3 bucket to store encrypted configuration'
+          )
+          c.option(
+            '-k',
+            '--kms-key STRING',
+            'The KMS key to use for encryption'
+          )
+          c.option(
+            '-e',
+            '--ec2-key STRING',
+            'The EC2 key pair to associate with EC2 instances'
+          )
           c.option(
             '-z',
             '--hosted-zone-id STRING',
             'The Route53 Hosted Zone ID for the public hosted zone'
           )
+          c.option(
+            '-o',
+            '--organization STRING',
+            'The organization to create on the Chef Server'
+          )
+          c.option(
+            '-u',
+            '--username STRING',
+            'The username to create on the Chef Server'
+          )
+          c.option(
+            '-m',
+            '--email STRING',
+            'The email address for the Chef Server user'
+          )
+          c.option(
+            '-f',
+            '--first-name STRING',
+            'The first name of the Chef Server user'
+          )
+          c.option(
+            '-l',
+            '--last-name STRING',
+            'The last name of the Chef Server user'
+          )
+          c.option(
+            '-i',
+            '--instance-cookbook STRING',
+            'The instance cookbook to apply additional ' \
+            'configuration to the Chef Server'
+          )
+          c.option(
+            '-j',
+            '--targets-json JSON',
+            'The target specific configuration for the Chef Server'
+          )
         end
+        # rubocop:enable Metrics/MethodLength
 
         def bootstrap_directory(options)
           directory = options.directory || ask('Directory? ') do |q|
@@ -27,17 +81,88 @@ module Formatron
           end
         end
 
+        def bootstrap_s3_bucket(options)
+          options.s3_bucket || ask('S3 Bucket? ')
+        end
+
+        def bootstrap_kms_key(options)
+          options.kms_key || ask('KMS Key? ')
+        end
+
+        def bootstrap_ec2_key(options)
+          options.ec2_key || ask('EC2 Key? ')
+        end
+
         def bootstrap_hosted_zone_id(options)
           options.hosted_zone_id || ask('Hosted Zone ID? ')
         end
+
+        def bootstrap_organization(options)
+          options.organization || ask('Organization? ')
+        end
+
+        def bootstrap_username(options)
+          options.username || ask('Username? ')
+        end
+
+        def bootstrap_email(options)
+          options.email || ask('Email? ')
+        end
+
+        def bootstrap_first_name(options)
+          options.first_name || ask('First Name? ')
+        end
+
+        def bootstrap_last_name(options)
+          options.last_name || ask('Last Name? ')
+        end
+
+        def bootstrap_instance_cookbook(options)
+          options.instance_cookbook || ask('Instance Cookbook? ')
+        end
+
+        def bootstrap_ask_targets
+          params = {}
+          targets = ask 'Targets? ', Array
+          targets.each do |target|
+            params[target] = {}
+            params[target][:protected] = agree "#{target} protected? "
+            params[target][:sub_domain] = ask "#{target} sub domain? "
+            params[target][:password] = password "#{target} password? "
+          end
+          params
+        end
+
+        def bootstrap_targets(options)
+          json = options.targets_json
+          json ? JSON.parse(json.gsub!(/\A'|'\Z/, '')) : bootstrap_ask_targets
+        end
+
+        # rubocop:disable Metrics/MethodLength
+        def bootstrap_params(options, directory)
+          {
+            name: bootstrap_name(options, directory),
+            s3_bucket: bootstrap_s3_bucket(options),
+            kms_key: bootstrap_kms_key(options),
+            ec2_key: bootstrap_ec2_key(options),
+            hosted_zone_id: bootstrap_hosted_zone_id(options),
+            organization: bootstrap_organization(options),
+            username: bootstrap_username(options),
+            email: bootstrap_email(options),
+            first_name: bootstrap_first_name(options),
+            last_name: bootstrap_last_name(options),
+            instance_cookbook: bootstrap_instance_cookbook(options),
+            targets: bootstrap_targets(options)
+          }
+        end
+        # rubocop:enable Metrics/MethodLength
 
         def bootstrap_action(c)
           c.action do |_args, options|
             directory = bootstrap_directory options
             Bootstrap.generate(
               directory,
-              name: bootstrap_name(options, directory),
-              hosted_zone_id: bootstrap_hosted_zone_id(options)
+              bootstrap_params(options, directory)
             )
           end
         end
@@ -52,6 +177,7 @@ module Formatron
           end
         end
       end
+      # rubocop:enable Metrics/ModuleLength
     end
   end
 end
--- spec/formatron/generators/bootstrap/cli_spec.rb
+++ spec/formatron/generators/bootstrap/cli_spec.rb
@@ -4,6 +4,8 @@ require 'formatron/cli'
 require 'formatron/generators/bootstrap/cli'
 
 describe Formatron::Generators::Bootstrap::CLI do
+  include FakeFS::SpecHelpers
+
   # Test harness
   class Test < Formatron::CLI
     include Formatron::Generators::Bootstrap::CLI
@@ -11,44 +13,89 @@ describe Formatron::Generators::Bootstrap::CLI do
 
   directory = 'directory'
   name = 'test'
+  s3_bucket = 's3-bucket'
+  kms_key = 'kms-key'
+  ec2_key = 'ec2-key'
   hosted_zone_id = 'ABCDEF'
+  organization = 'organization'
+  username = 'username'
+  email = 'email'
+  first_name = 'first-name'
+  last_name = 'last-name'
+  instance_cookbook = 'instance-cookbook'
+  targets = %w(target1 target2)
+  target_params = {}
+  targets.each do |target|
+    target_params[target] = {}
+    target_params[target][:protected] = true
+    target_params[target][:sub_domain] = "#{target}-sub-domain"
+    target_params[target][:password] = "#{target}-password"
+  end
 
   expected_params = {
     name: name,
-    hosted_zone_id: hosted_zone_id
+    s3_bucket: s3_bucket,
+    kms_key: kms_key,
+    ec2_key: ec2_key,
+    hosted_zone_id: hosted_zone_id,
+    organization: organization,
+    username: username,
+    email: email,
+    first_name: first_name,
+    last_name: last_name,
+    instance_cookbook: instance_cookbook,
+    targets: target_params
   }
 
   before(:each) do
-    @bootstrap = class_double(
-      'Formatron::Generators::Bootstrap'
-    ).as_stubbed_const
+    lib = File.expand_path(
+      File.join(
+        File.dirname(File.expand_path(__FILE__)),
+        '../../../../lib'
+      )
+    )
+    FakeFS::FileSystem.clone lib
   end
 
   context 'with no options' do
     before(:each) do
       allow(Commander::Runner).to receive(:instance) do
-        @singleton ||= Commander::Runner.new ['bootstrap', '-t']
+        @singleton ||= Commander::Runner.new ['bootstrap']
       end
-      @input = StringIO.new <<-EOH.gsub(/^ {8}/, '')
+    end
+
+    it 'should prompt for parameters' do
+      responses = <<-EOH.gsub(/^ {8}/, '')
         #{directory}
         #{name}
+        #{s3_bucket}
+        #{kms_key}
+        #{ec2_key}
         #{hosted_zone_id}
+        #{organization}
+        #{username}
+        #{email}
+        #{first_name}
+        #{last_name}
+        #{instance_cookbook}
+        #{targets.join(' ')}
       EOH
+      targets.each do |target|
+        responses << <<-EOH.gsub(/^ {10}/, '')
+          #{target_params[target][:protected] ? 'yes' : 'no'}
+          #{target_params[target][:sub_domain]}
+          #{target_params[target][:password]}
+        EOH
+      end
+      @input = StringIO.new responses
       @output = StringIO.new
       # rubocop:disable Style/GlobalVars
       $terminal = HighLine.new @input, @output
       # rubocop:enable Style/GlobalVars
-    end
-
-    after(:each) do
-      puts @output.read
-    end
-
-    it 'should prompt for parameters' do
-      expect(@bootstrap).to receive(:generate).once.with(
+      expect(Formatron::Generators::Bootstrap).to receive(:generate).once.with(
         File.expand_path('directory'),
         expected_params
-      )
+      ).and_call_original
       Test.new.run
     end
   end
@@ -56,15 +103,71 @@ describe Formatron::Generators::Bootstrap::CLI do
   context 'with all short form options' do
     before(:each) do
       allow(Commander::Runner).to receive(:instance) do
-        @singleton ||= Commander::Runner.new ['bootstrap',
-                                              '-d', directory,
-                                              '-n', name,
-                                              '-z', hosted_zone_id]
+        @singleton ||=
+          Commander::Runner.new [
+            'bootstrap',
+            '-d', directory,
+            '-n', name,
+            '-s', s3_bucket,
+            '-k', kms_key,
+            '-e', ec2_key,
+            '-z', hosted_zone_id,
+            '-o', organization,
+            '-u', username,
+            '-m', email,
+            '-f', first_name,
+            '-l', last_name,
+            '-i', instance_cookbook,
+            '-j', "'#{target_params.to_json space: ' '}'"
+          ]
       end
     end
 
     it 'should call generate' do
-      expect(@bootstrap).to receive(:generate).once.with(
+      @input = StringIO.new
+      @output = StringIO.new
+      # rubocop:disable Style/GlobalVars
+      $terminal = HighLine.new @input, @output
+      # rubocop:enable Style/GlobalVars
+      expect(Formatron::Generators::Bootstrap).to receive(:generate).once.with(
+        File.expand_path('directory'),
+        expected_params
+      )
+      Test.new.run
+    end
+  end
+
+  context 'with all long form options' do
+    before(:each) do
+      allow(Commander::Runner).to receive(:instance) do
+        @singleton ||=
+          Commander::Runner.new [
+            'bootstrap',
+            '-t',
+            '--directory', directory,
+            '--name', name,
+            '--s3-bucket', s3_bucket,
+            '--kms-key', kms_key,
+            '--ec2-key', ec2_key,
+            '--hosted-zone-id', hosted_zone_id,
+            '--organization', organization,
+            '--username', username,
+            '--email', email,
+            '--first-name', first_name,
+            '--last-name', last_name,
+            '--instance-cookbook', instance_cookbook,
+            '--targets-json', "'#{target_params.to_json space: ' '}'"
+          ]
+      end
+    end
+
+    it 'should call generate' do
+      @input = StringIO.new
+      @output = StringIO.new
+      # rubocop:disable Style/GlobalVars
+      $terminal = HighLine.new @input, @output
+      # rubocop:enable Style/GlobalVars
+      expect(Formatron::Generators::Bootstrap).to receive(:generate).once.with(
         File.expand_path('directory'),
         expected_params
       )
