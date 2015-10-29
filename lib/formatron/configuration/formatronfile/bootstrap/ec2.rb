3da9c7040b2e222490e6b7783fcf416a71294b3d
Peter Halliday <pghalliday@gmail.com>
2015-10-29 15:56:20 +0100

    disappearing down a meta programming rabbit hole

--- lib/formatron/configuration/formatronfile/bootstrap/ec2.rb
+++ lib/formatron/configuration/formatronfile/bootstrap/ec2.rb
@@ -4,14 +4,14 @@ class Formatron
       class Bootstrap
         # EC2 key pair configuration
         class EC2
-          def key_pair(value = nil)
-            @key_pair = value unless value.nil?
-            @key_pair
-          end
-
-          def private_key(value = nil)
-            @private_key = value unless value.nil?
-            @private_key
+          %i(
+            key_pair
+            private_key
+          ).each do |symbol|
+            define_method symbol do |value = nil|
+              instance_variable_set "@#{symbol}", value unless value.nil?
+              instance_variable_get "@#{symbol}"
+            end
           end
         end
       end
--- lib/formatron/configuration/formatronfile/bootstrap/nat.rb
+++ lib/formatron/configuration/formatronfile/bootstrap/nat.rb
@@ -4,19 +4,15 @@ class Formatron
       class Bootstrap
         # NAT instance configuration
         class NAT
-          def subnet(value = nil)
-            @subnet = value unless value.nil?
-            @subnet
-          end
-
-          def sub_domain(value = nil)
-            @sub_domain = value unless value.nil?
-            @sub_domain
-          end
-
-          def instance_cookbook(value = nil)
-            @instance_cookbook = value unless value.nil?
-            @instance_cookbook
+          %i(
+            subnet
+            sub_domain
+            instance_cookbook
+          ).each do |symbol|
+            define_method symbol do |value = nil|
+              instance_variable_set "@#{symbol}", value unless value.nil?
+              instance_variable_get "@#{symbol}"
+            end
           end
         end
       end
