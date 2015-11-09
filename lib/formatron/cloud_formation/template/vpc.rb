48b02051b9409ab67274407d59ba58b3596c6826
Peter Halliday <pghalliday@gmail.com>
2015-11-09 01:13:41 +0100

    added VPC resources

--- lib/formatron/cloud_formation/template/vpc.rb
+++ lib/formatron/cloud_formation/template/vpc.rb
@@ -1,4 +1,5 @@
 require_relative 'vpc/subnet'
+require 'formatron/cloud_formation/template/resources/ec2'
 
 class Formatron
   module CloudFormation
@@ -6,17 +7,82 @@ class Formatron
       # generates CloudFormation VPC resources
       class VPC
         PREFIX = 'vpc'
+        INTERNET_GATEWAY_PREFIX = 'internetGateway'
+        VPC_GATEWAY_ATTACHMENT_PREFIX = 'vpcGatewayAttachment'
+        PUBLIC_ROUTE_TABLE_PREFIX = 'publicRouteTable'
+        PUBLIC_ROUTE_PREFIX = 'publicRoute'
 
+        # rubocop:disable Metrics/MethodLength
         def initialize(vpc:)
           @vpc = vpc
+          @cidr = vpc.cidr
+          @guid = vpc.guid
+          @logical_id = "#{PREFIX}#{@guid}"
+          @internet_gateway_id = "#{INTERNET_GATEWAY_PREFIX}#{@guid}"
+          @vpc_gateway_attachment_id =
+            "#{VPC_GATEWAY_ATTACHMENT_PREFIX}#{@guid}"
+          @public_route_table_id =
+            "#{PUBLIC_ROUTE_TABLE_PREFIX}#{@guid}"
+          @public_route_id =
+            "#{PUBLIC_ROUTE_PREFIX}#{@guid}"
         end
+        # rubocop:enable Metrics/MethodLength
 
         def merge(resources:, outputs:)
           @vpc.subnet.each do |_, subnet|
             template_subnet = Subnet.new subnet: subnet
             template_subnet.merge resources: resources, outputs: outputs
           end
+          _add_vpc resources, outputs
+          _add_internet_gateway resources
+          _add_vpc_gateway_attachment resources
+          _add_public_route_table resources
+          _add_public_route resources
         end
+
+        def _add_vpc(resources, outputs)
+          resources[@logical_id] = Resources::EC2.vpc cidr: @cidr
+          outputs[@logical_id] = Template.output Template.ref(@logical_id)
+        end
+
+        def _add_internet_gateway(resources)
+          resources[@internet_gateway_id] = Resources::EC2.internet_gateway
+        end
+
+        def _add_vpc_gateway_attachment(resources)
+          resources[
+            @vpc_gateway_attachment_id
+          ] = Resources::EC2.vpc_gateway_attachment(
+            vpc: @logical_id,
+            gateway: @internet_gateway_id
+          )
+        end
+
+        def _add_public_route_table(resources)
+          resources[
+            @public_route_table_id
+          ] = Resources::EC2.route_table(
+            vpc: @logical_id
+          )
+        end
+
+        def _add_public_route(resources)
+          resources[
+            @public_route_id
+          ] = Resources::EC2.route(
+            vpc_gateway_attachment: @vpc_gateway_attachment_id,
+            internet_gateway: @internet_gateway_id,
+            route_table: @public_route_table_id
+          )
+        end
+
+        private(
+          :_add_vpc,
+          :_add_internet_gateway,
+          :_add_vpc_gateway_attachment,
+          :_add_public_route_table,
+          :_add_public_route
+        )
       end
     end
   end
--- spec/formatron/cloud_formation/template/vpc_spec.rb
+++ spec/formatron/cloud_formation/template/vpc_spec.rb
@@ -20,16 +20,87 @@ class Formatron
           allow(formatronfile_vpc).to receive(
             :subnet
           ) { @formatronfile_instances[:subnet] }
+          @ec2 = class_double(
+            'Formatron::CloudFormation::Template::Resources::EC2'
+          ).as_stubbed_const
+          guid = 'guid'
+          allow(formatronfile_vpc).to receive(:guid) { guid }
+          cidr = 'cidr'
+          allow(formatronfile_vpc).to receive(:cidr) { cidr }
+          @vpc = 'vpc'
+          allow(@ec2).to receive(:vpc).with(
+            cidr: cidr
+          ) { @vpc }
+          @internet_gateway = 'internet_gateway'
+          allow(@ec2).to receive(:internet_gateway) { @internet_gateway }
+          @logical_id = "vpc#{guid}"
+          @internet_gateway_id = "internetGateway#{guid}"
+          @vpc_gateway_attachment_id = "vpcGatewayAttachment#{guid}"
+          @vpc_gateway_attachment = 'vpc_gateway_attachment'
+          allow(@ec2).to receive(:vpc_gateway_attachment).with(
+            vpc: @logical_id,
+            gateway: @internet_gateway_id
+          ) { @vpc_gateway_attachment }
+          @public_route_table_id = "publicRouteTable#{guid}"
+          @public_route_table = 'public_route_table'
+          allow(@ec2).to receive(:route_table).with(
+            vpc: @logical_id
+          ) { @public_route_table }
+          @public_route_id = "publicRoute#{guid}"
+          @public_route = 'public_route'
+          allow(@ec2).to receive(:route).with(
+            vpc_gateway_attachment: @vpc_gateway_attachment_id,
+            route_table: @public_route_table_id,
+            internet_gateway: @internet_gateway_id
+          ) { @public_route }
           @template_vpc = VPC.new vpc: formatronfile_vpc
         end
 
         describe '#merge' do
+          before :each do
+            @resources = {}
+            @outputs = {}
+            @template_vpc.merge resources: @resources, outputs: @outputs
+          end
+
           it 'should add the subnets' do
-            resources = {}
-            outputs = {}
-            @template_vpc.merge resources: resources, outputs: outputs
-            expect(resources).to eql @results
-            expect(outputs).to eql @results
+            expect(@resources).to include @results
+            expect(@outputs).to include @results
+          end
+
+          it 'should add a VPC' do
+            expect(@resources).to include(
+              @logical_id => @vpc
+            )
+            expect(@outputs).to include(
+              @logical_id => {
+                Value: { Ref: @logical_id }
+              }
+            )
+          end
+
+          it 'should add an internet gateway' do
+            expect(@resources).to include(
+              @internet_gateway_id => @internet_gateway
+            )
+          end
+
+          it 'should add a VPC gateway attachment' do
+            expect(@resources).to include(
+              @vpc_gateway_attachment_id => @vpc_gateway_attachment
+            )
+          end
+
+          it 'should add a public route table' do
+            expect(@resources).to include(
+              @public_route_table_id => @public_route_table
+            )
+          end
+
+          it 'should add a public route' do
+            expect(@resources).to include(
+              @public_route_id => @public_route
+            )
           end
         end
       end
